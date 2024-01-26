24a097dcc5d4c8ad889763ada1c44c8a777851f0
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2024-01-26 21:18:25 +0200

    get meal details ..

--- js/java.js
+++ js/java.js
@@ -21,7 +21,6 @@ $(".open-drawer").click(function (e) {
 
     $(".container-nav").animate({
       width: "20%",
-      
     });
     $(element).attr(
       "class",
@@ -34,10 +33,7 @@ $(".open-drawer").click(function (e) {
 
     $(".container-nav").animate({
       width: "4%",
-      
     });
-
-   
 
     $(element).attr(
       "class",
@@ -91,9 +87,6 @@ $(".show-search-controller").click(function (e) {
   $(".body-data").html("");
 
   close();
-
-  
-
 });
 
 async function getCategoryMeals() {
@@ -119,6 +112,75 @@ async function getCategoryMeals() {
       </div>
     `);
   }
+  $(".place-holder").fadeOut(300);
+}
+
+async function getMealDetails(id) {
+  console.log(id);
+  close();
+  $(".body-data").html("");
+  $(".place-holder").fadeIn(300);
+
+  let respone = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+  result = await respone.json();
+
+  const ingredientsLength = Object.keys(result.meals[0]).filter(
+    (key) => key.startsWith("strIngredient") && result.meals[0][key] !== ""
+  ).length;
+
+  let containerOfIngredients = ``;
+
+  for (let i = 1; i <= ingredientsLength; i++) {
+    if (result.meals[0][`strIngredient${i}`]) {
+      containerOfIngredients += `<li class="alert alert-info m-2 p-1">${
+        result.meals[0][`strMeasure${i}`]
+      } ${result.meals[0][`strIngredient${i}`]}</li>`;
+    }
+  }
+
+  console.info(ingredientsLength);
+
+  let tagsStr = "";
+
+  if (result.meals[0].strTags == null) {
+    console.log("Nulll Tags")
+  } else {
+    for (let i = 0; i < result.meals[0].strTags.split(",").length; i++) {
+      tagsStr += `
+        <li class="alert alert-danger m-2 p-1">${
+          result.meals[0].strTags.split(",")[i]
+        }</li>`;
+    }
+    console.info(result.meals[0].strTags.split(","));
+  }
+
+  $(".body-data").append(`
+  <div class="col-md-4 text-white ">
+              <img class="w-100 rounded-3" src="${result.meals[0].strMealThumb}"
+                  alt="">
+                  <h2>${result.meals[0].strMeal}</h2>
+          </div>
+          <div class="col-md-8 text-white">
+              <h2>Instructions</h2>
+              <p>${result.meals[0].strInstructions}</p>
+              <h3><span class="fw-bolder">Area : </span>${result.meals[0].strArea}</h3>
+              <h3><span class="fw-bolder">Category : </span>${result.meals[0].strCategory}</h3>
+              <h3>Recipes :</h3>
+              <ul class="list-unstyled d-flex g-3 flex-wrap">
+              ${containerOfIngredients}
+               </ul>
+
+               <h3>Tags :</h3>
+               <ul class="list-unstyled d-flex g-3 flex-wrap">
+                   ${tagsStr}
+               </ul>
+             
+
+              <a class="btn btn-success"  href="${result.meals[0].strSource}" target="_blank">Source</a>
+              <a class="btn btn-danger"   href="${result.meals[0].strYoutube}" target="_blank"  >Youtube</a>
+          </div>`);
   $(".place-holder").fadeOut(300);
 }
 
